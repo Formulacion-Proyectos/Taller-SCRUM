33e63f3cb7c9d6a54e0f3406579007d07caf2e7e
jctrejosi@unal.edu.co <jctrejosi@unal.edu.co>
2022-06-09 23:27:52 -0500

    Explicación vue 2

---
+++ src/views/InitialPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1
+    v-for="(item, i) in usuarios"
+    :key="i"
+  >
+    {{ item.direccion.calle }} - {{ item.direccion.carrera }}
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'InitialPage',
+
+  data () {
+    return {
+      usuarios: [
+        {
+          nombre: 'pepito',
+          apellido: 'perez',
+          direccion: {
+            calle: 'tal',
+            carrera: 'pascual'
+          }
+        },
+        {
+          nombre: 'otro pepito',
+          apellido: 'otro follajes',
+          direccion: {
+            calle: 'tal 2',
+            carrera: 'pascual 2'
+          }
+        }
+      ]
+    }
+  },
+
+  methods: {
+    sumar () {
+      this.numero ++
+      console.log(this.numero)
+    }
+  }
+}
+</script>
