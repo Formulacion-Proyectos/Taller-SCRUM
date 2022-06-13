46de2c754c006a31a2e95b5b069a0b772860f7ad
JUSERAG <jsramirezag@unal.edu.co>
2022-06-12 22:19:34 -0500

    se agrega el componente de ofertas con la implementación de un carrusel, se agregan las imagenes correspondientes

---
+++ src/components/offerModule.vue
@@ -0,0 +1,53 @@
+<template>
+<div class='container'>
+    <div id="carouselExampleFade" class="carousel slide carousel-fade col-6" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="4000">
+      <img src="../assets/carrusel/conjunto1.jpg" class="d-block w-100 img-thumbnail"  alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Mira nuestros nuevos conjuntos</h5>
+        <p><button type="button" class="btn btn-light">Ver más </button></p>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="4000">
+      <img src="../assets/carrusel/promociones.jpg" class="d-block w-100 img-thumbnail" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Promociones disponibles</h5>
+        <p><button type="button" class="btn btn-light">Ver más </button></p>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="4000">
+      <img src="../assets/carrusel/conjunto.jpg" class="d-block w-100 img-thumbnail" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Eres arte viste arte</h5>
+        <p><button type="button" class="btn btn-light">Ver más </button></p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+</script>
+
+<!-- <style >
+    .carousel-item{
+        $carousel-transition-duration: .1s;
+    }
+
+</style> -->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,10 @@
 <template>
+  <Offer />
   <buttonDropdown />
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import buttonDropdown from '../components/buttonDropdown.vue'
+import Offer from '../components/offerModule.vue'
 </script>
