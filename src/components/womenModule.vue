46c87e3f5ff4fd903f9b1e5e1923c27318b2b7ff
dimartinezp <dimartinezp@unal.edu.co>
2022-06-14 13:19:27 -0500

    se añade modulo de ropa y accesorios de mujer con sus respectivas imagenes e información

---
+++ src/components/womenModule.vue
@@ -0,0 +1,135 @@
+
+<template>
+
+  <div class="album py-5 bg-light">
+   <div class="container">
+
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card shadow-sm">
+            <img src = "src\assets\Camisasyblusas-NATURAL-E172426-2.webp">
+            <div class="card-body">
+              <h4 class="card-title">Blusa Manga sisa.</h4>
+              <p class="card-text">Con textura de encaje.</p>
+              <div class="d-flex justify-content-between align-items-center">
+              <p class="card-text">$ 58.000</p>
+                <div class="btn-group">
+                  <a href="" class="btn btn-primary">Detalles</a>
+                </div>
+                <div class="btn-group">
+                    <a href="" class="btn btn-success">Agrerar</a>
+                </div>
+                <small class="text-muted">Nuevo</small>
+               </div>
+             </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+            <img src = "src\assets\-elaco-producto-Buzos-NEGROBLANCO-E262521-1.webp">
+            <div class="card-body">
+              <h4 class="card-title">Buso tejido a cuadros.</h4>
+              <p class="card-text">Hermoso buso tejido a cuadros con textura.</p>
+              <div class="d-flex justify-content-between align-items-center">
+              <p class="card-text">$ 98.250</p>
+                <div class="btn-group">
+                  <a href="" class="btn btn-primary">Detalles</a>
+                </div>
+                <div class="btn-group">
+                    <a href="" class="btn btn-success">Agrerar</a>
+                </div>
+                <small class="text-muted">Nuevo</small>
+               </div>
+             </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+            <img src = "src\assets\-elaco-producto-Vestidos-NATURAL-E141103-1.webp">
+            <div class="card-body">
+              <h4 class="card-title">Vestido corto en flores.</h4>
+              <p class="card-text">manga 3/4 con volumen .</p>
+              <div class="d-flex justify-content-between align-items-center">
+              <p class="card-text">$ 111.900</p>
+                <div class="btn-group">
+                  <a href="" class="btn btn-primary">Detalles</a>
+                </div>
+                <div class="btn-group">
+                    <a href="" class="btn btn-success">Agrerar</a>
+                </div>
+                <small class="text-muted">Nuevo</small>
+               </div>
+             </div>
+          </div>
+        </div>
+
+                <div class="col">
+          <div class="card shadow-sm">
+            <img src = "src\assets\botass.webp">
+            <div class="card-body">
+              <h4 class="card-title">Bota media caña Militar</h4>
+              <p class="card-text">En charol y con cordones.</p>
+              <div class="d-flex justify-content-between align-items-center">
+              <p class="card-text">$ 150.000</p>
+                <div class="btn-group">
+                  <a href="" class="btn btn-primary">Detalles</a>
+                </div>
+                <div class="btn-group">
+                    <a href="" class="btn btn-success">Agrerar</a>
+                </div>
+                <small class="text-muted">Nuevo</small>
+               </div>
+             </div>
+          </div>
+        </div>
+
+                <div class="col">
+          <div class="card shadow-sm">
+            <img src = "src\assets\bolso.webp">
+            <div class="card-body">
+              <h4 class="card-title">Bolso Hombro Herraje En Frontal</h4>
+              <p class="card-text">Hecho de cuero vegano.</p>
+              <div class="d-flex justify-content-between align-items-center">
+              <p class="card-text">$ 105.990</p>
+                <div class="btn-group">
+                  <a href="" class="btn btn-primary">Detalles</a>
+                </div>
+                <div class="btn-group">
+                    <a href="" class="btn btn-success">Agrerar</a>
+                </div>
+                <small class="text-muted">Nuevo</small>
+               </div>
+             </div>
+          </div>
+        </div>
+
+                <div class="col">
+          <div class="card shadow-sm">
+            <img src = "src\assets\Blusa.webp">
+            <div class="card-body">
+              <h4 class="card-title">Blusa Manga Larga Con Estampado Combinado</h4>
+              <p class="card-text">Con mangas de encaje y voluminizadoras.</p>
+              <div class="d-flex justify-content-between align-items-center">
+              <p class="card-text">$ 70.900</p>
+                <div class="btn-group">
+                  <a href="" class="btn btn-primary">Detalles</a>
+                </div>
+                <div class="btn-group">
+                    <a href="" class="btn btn-success">Agrerar</a>
+                </div>
+                <small class="text-muted">Nuevo</small>
+               </div>
+             </div>
+          </div>
+        </div>
+    </div>
+  </div>
+  </div>
+
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <buttonDropdown />
+  <womenModule/>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import buttonDropdown from '../components/buttonDropdown.vue'
+import womenModule from '../components/womenModule.vue'
 </script>
