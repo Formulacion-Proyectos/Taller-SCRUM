09184cdc3e5bb086d6fecf487370763b8ad14413
jctrejosi@unal.edu.co <jctrejosi@unal.edu.co>
2022-06-15 13:39:25 -0500

    Correct carousel

--- src/components/offerModule.vue
+++ src/components/offerModule.vue
@@ -2,21 +2,21 @@
 <div class='container'>
     <div id="carouselExampleFade" class="carousel slide carousel-fade col-6" data-bs-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="4000">
+    <div class="carousel-item active" data-bs-interval="2200">
       <img src="../assets/carrusel/conjunto1.jpg" class="d-block w-100 img-thumbnail"  alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>Mira nuestros nuevos conjuntos</h5>
         <p><button type="button" class="btn btn-light">Ver más </button></p>
       </div>
     </div>
-    <div class="carousel-item" data-bs-interval="4000">
+    <div class="carousel-item" data-bs-interval="2200">
       <img src="../assets/carrusel/promociones.jpg" class="d-block w-100 img-thumbnail" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>Promociones disponibles</h5>
         <p><button type="button" class="btn btn-light">Ver más </button></p>
       </div>
     </div>
-    <div class="carousel-item" data-bs-interval="4000">
+    <div class="carousel-item" data-bs-interval="2200">
       <img src="../assets/carrusel/conjunto.jpg" class="d-block w-100 img-thumbnail" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>Eres arte viste arte</h5>
@@ -40,6 +40,19 @@
 
 #carouselExampleFade {
   width: 100%;
+}
+
+.carousel-item {
+  height: 80rem;
+}
+
+img {
+  object-fit: cover;
+  height: 100%;
+}
+
+h5 {
+  background-color: rgba(0, 0, 0, .5);
 }
 
 @media (min-width: 1200px) {
